93b7b29b440ac45110a113b9bb767e22649e8636
Shuga Agemura <sagemura@c1r13s6.42tokyo.jp>
2024-02-19 21:14:06 +0900

    update

--- ft_printf/tr02/ft_printf.c
+++ ft_printf/tr02/ft_printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -17,9 +18,45 @@ int print_string(char *str)
 	return (write(1, str, ft_strlen(str)));
 }
 
+void print_hex(unsigned int n, size_t *i)
+{
+	char *base;
+
+	base = "0123456789abcdef";
+	if(n >= 16)
+	{
+		print_hex(n / 16, i);
+		print_hex(n % 16, i);
+	}
+	else
+	{
+		write(1, &base[n], 1);
+		(*i)++;
+	}
+}
+
+int print_nbr(long long n)
+{
+	char *nbr;
+
+	nbr = "0123456789";
+	
+	if(n < 0)
+	{
+		write(1, "-", 1);
+		if(n <= -10)
+			print_nbr(n/ 10);
+		return(write(1, &nbr[-(n % 10)], 1) + 1);
+	}
+	else if(n >= 10)
+		print_nbr(n/ 10);
+	return(write(1, &nbr[n % 10], 1));
+}
+
+
 int ft_printf(const char *format, ...)
 {
-	int i;	
+	size_t  i;	
 	va_list args;
 
 	i = 0;
@@ -32,9 +69,27 @@ int ft_printf(const char *format, ...)
 			if(*format == 's')
 				i += print_string(va_arg(args, char *));
 			if(*format == 'd')	
-				i += print_string(ft_itoa(va_arg(args, int)));
-					
+				i += print_nbr (va_arg(args, long long));
+			if(*format == 'x')
+				 print_hex(va_arg(args, unsigned int), &i);
 		}
+		else
+		{
+			write(1, format, 1);
+			i++;
+		}
+		format++;
 	}
-
+	va_end(args);
+	return(i);
 }
+
+
+int main(void)
+{
+	printf("Hello %s\n%d\n%x\n%d\n", "world", 42, 42, printf("%x", 42));	
+	ft_printf("Hello %s\n%d\n%x\n%d\n", "world", 42, 42,ft_printf("%x", 42));	
+	return (0);
+}
+
+
